a352379004894bdbf9c32e7c7ba238a7606255a1
Lucas <lucaslfurini@gmail.com>
2024-08-11 23:08:36 +0200

    chore: test

--- app/page.tsx
+++ app/page.tsx
@@ -32,7 +32,7 @@ export default function Home() {
       >
         <section className="relative mx-auto flex max-w-7xl flex-col items-center p-8 pb-64 pt-64">
           <h2 className="flex flex-col text-center text-7xl font-medium text-white">
-            A maneira mais fácil,{" "}
+            A maneira mais fácil e rápida{" "}
             <span className="bg-gradient-to-r from-yellow-300 from-20% via-yellow-400 to-yellow-600 to-70% bg-clip-text pb-6 text-transparent">
               De gerenciar todos os seus processos
             </span>
